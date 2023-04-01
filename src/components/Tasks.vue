46f666e200d7a5c6b808b08842cefb0f4399f441
armin <genius2001armin@gmail.com>
2023-04-01 15:13:11 +0330

    create task components

---
+++ src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+    <div
+      v-for="task in tasks"
+      :key="task.text"
+      >
+      <h3> {{task.text}} </h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    tasks: Array
+  }
+}
+</script>
