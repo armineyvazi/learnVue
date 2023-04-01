33dc545b3bb6bdc46c6fb7e8e7e90505c29e57e4
armin <genius2001armin@gmail.com>
2023-04-01 15:14:19 +0330

    change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,9 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
     'vue/no-reserved-component-names': 'off',
-    'vue/no-multiple-empty-lines': 'off'
+    'vue/no-multiple-empty-lines': 'off',
+    // 'vue/valid-template-root': 'off',
+    'no-trailing-spaces': 'off',
+    'eol-last': 'off'
   }
 }
