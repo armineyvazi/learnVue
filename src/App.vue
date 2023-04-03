a1a3ceb587cb2d9a07bbe68ac17445514d68d40c
armin <genius2001armin@gmail.com>
2023-04-03 23:59:58 +0330

    Add Task components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
   <div class="container">
-      <Header title="task maneger"/>
+      <Header title="Task Tracker"/>
+      <Tasks @toggle-reminder="toggleReminder" 
+             @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask (id) {
+      if (confirm('Are you sure')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder (id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)
+    }
+  },
+  created () {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Doctor Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Doctor Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: false
+      }
+    ]
   }
 }
 
